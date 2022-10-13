15189a847f4e38f435bcd6c5cd7063a556a1c77c
RafayelGasparyan <rafogasparyan50@gmail.com>
2022-10-13 20:26:53 +0400

    project first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TEXT</h1>
+      <h2>Text</h2>
+    </div>
+  );
+}
+
+export default App;
